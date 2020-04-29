0c71cd623711f1b45eda421e3f2c8011e6d69da1
Rastam Hadi <rastam.hadi@gmail.com>
2020-04-30 05:45:43 +0900

    Setup Github Action

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,5 +5,8 @@ require 'test_helper'
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include FactoryBot::Syntax::Methods
 
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome do |driver_options|
+    driver_options.add_argument('--disable-dev-sim-usage')
+    driver_options.add_argument('--no-sandbox')
+  end
 end
