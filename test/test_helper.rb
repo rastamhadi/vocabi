7a13cc722a71ebca55641d54d3fcd9cddb73ee09
Rastam Hadi <rastam.hadi@gmail.com>
2020-07-26 06:49:15 +0900

    Setup VCR

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,15 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'webmock/minitest'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.hook_into(:webmock)
+  config.ignore_localhost = true
+  config.ignore_hosts('chromedriver.storage.googleapis.com')
+end
 
 class Minitest::Unit::TestCase
   include FactoryBot::Syntax::Methods
