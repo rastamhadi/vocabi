3c3fc77e69374d57f98d9b1557fe84d5ff18a851
Rastam Hadi <rastam.hadi@gmail.com>
2020-08-11 02:46:32 +0900

    rails webpacker:install:stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("./bootstrap")
+
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+
+const application = Application.start();
+const context = require.context("../controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
